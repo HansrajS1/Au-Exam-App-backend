68544f259cc9bac0cf0a229dd9bc0519a612342d
HANS RAJ <155075212+HansrajS1@users.noreply.github.com>
2025-09-04 23:35:16 +0530

    add file

---
+++ src/test/java/com/AU/Exam/App/AuExamAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AU.Exam.App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuExamAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
